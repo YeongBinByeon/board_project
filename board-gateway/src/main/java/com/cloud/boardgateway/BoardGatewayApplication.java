a797f12df7cfb97ef836e2edc3ee900f036cf3ea
yeongbin <yeongbin6483@gmail.com>
2022-10-11 07:38:09 +0900

    기존 게시판 프로젝트에 discovery, api gateway, cloud config 서비스들 연결 설정하여 커밋

--- board-gateway/src/main/java/com/cloud/boardgateway/BoardGatewayApplication.java
+++ board-gateway/src/main/java/com/cloud/boardgateway/BoardGatewayApplication.java
@@ -2,8 +2,10 @@ package com.cloud.boardgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BoardGatewayApplication {
 
 	public static void main(String[] args) {
